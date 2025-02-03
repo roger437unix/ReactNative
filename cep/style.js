b184d02a177ff47711c6a6728376e91f39c72340
roger <roger@localhost.localdomain>
2025-02-02 22:59:25 -0300

    Add app cep

---
+++ cep/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+// Criando grupo de estilos
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#77eaea",
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: "center"
+    },
+    texto: {
+        fontSize: 20,
+        color: '#5500ff'
+    }
+})
