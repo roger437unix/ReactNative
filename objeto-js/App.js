11e3bfe514650d75399cedd5910af5fd79645933
roger <roger@localhost.localdomain>
2025-08-14 22:38:53 -0300

    Atividade sobre manipulação de Json da folha de estilos

---
+++ objeto-js/App.js
@@ -0,0 +1,79 @@
+// Atividade para turma do terceiro semestre
+
+import { Text, View, TouchableOpacity } from 'react-native';
+import { use, useState } from 'react';
+import { styles } from './style';
+
+
+export default function App() {
+
+  const [corDeFundo, setCorDeFundo] = useState('');
+  const [largura, setLargura] = useState();
+  const [altura, setAltura] = useState();
+  const [borda, setBorda] = useState();
+
+  const dados = (propriedades) => {
+    for (let i in propriedades) {
+      switch (i) {
+        case 'backgroundColor':
+          setCorDeFundo(`${i} : ${propriedades[i].slice(1, 7, 8)}`);
+          break;
+        case 'width':
+          setLargura(`${i} : ${propriedades[i]}`);
+          break;
+        case 'height':
+          setAltura(`${i} : ${propriedades[i]}`);
+          break;
+        case 'borderRadius':
+          setBorda(`${i} : ${propriedades[i]}`);
+      }
+    }
+  }
+
+  const clear = () => {
+    setCorDeFundo('');
+    setLargura('');
+    setAltura('');
+    setBorda('');
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <View style={styles.ladoalado}>
+        <View style={styles.area}>
+          <TouchableOpacity style={styles.btn1} onPress={() => { dados(styles.btn1) }}>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.area}>
+          <TouchableOpacity style={styles.btn2} onPress={() => { dados(styles.btn2) }}>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.ladoalado}>
+        <View style={styles.area}>
+          <TouchableOpacity style={styles.btn3} onPress={() => { dados(styles.btn3) }}>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.area}>
+          <TouchableOpacity style={styles.btn4} onPress={() => { dados(styles.btn4) }}>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={styles.output}>
+        <Text style={styles.texto}>{corDeFundo}</Text>
+        <Text style={styles.texto}>{largura}</Text>
+        <Text style={styles.texto}>{altura}</Text>
+        <Text style={styles.texto}>{borda}</Text>
+
+        <TouchableOpacity style={styles.btnClear} onPress={clear}>
+          <Text style={styles.textBtnClear}>Limpar</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    </View>
+  );
+}
---
+++ objeto-js/style.js
@@ -0,0 +1,82 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 25,        
+    },
+    ladoalado: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    area: {
+        flex: 1,
+        borderWidth: 5,
+        borderColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    btn1: {
+        backgroundColor: '#d72356',
+        width: '100%',
+        height: '100%',
+        borderRadius: 40,
+        borderWidth: 4,
+        borderColor: '#6d200fff',
+    },
+    btn2: {
+        backgroundColor: '#23d7b0ff',
+        width: 160,
+        height: 160,
+        borderRadius: 0,
+        borderWidth: 4,
+        borderColor: '#0a3704ff',
+    },
+    btn3: {
+        backgroundColor: '#2359d7ff',
+        width: 160,
+        height: 160,
+        borderRadius: 100,
+        borderWidth: 4,
+        borderColor: '#180f6dff',
+    },
+    btn4: {
+        backgroundColor: '#ffff25ff',
+        width: '100%',
+        height: 120,
+        borderRadius: 1,
+        borderWidth: 4,
+        borderColor: '#675d05ff',
+    },
+    output: {
+        flex: 1,
+        backgroundColor: '#ded7d7ff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 10,
+        borderColor: '#072405ff',
+        borderRadius: 50,
+        marginTop: 20,
+        marginBottom: 30,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    texto: {
+        fontSize: 22,
+        fontWeight: 'bold'
+    },
+    btnClear: {
+        backgroundColor: '#f5790dff',
+        width: '60%',
+        height: 50,
+        borderRadius: 18,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 40
+    },
+    textBtnClear: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    }
+});
