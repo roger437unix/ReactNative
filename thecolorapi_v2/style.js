6576711da41361e973dfc3b595feadb5b6d0b03f
ROGERIO DA SILVA ALCANTARA <sn1024493@educ123.sp.senai.br>
2025-08-22 09:04:55 -0300

    Alterando thecolorapi_v2

--- thecolorapi_v2/style.js
+++ thecolorapi_v2/style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#747679ff',
+        backgroundColor: '#ccd0d4ff',
         alignItems: 'center',
         justifyContent: 'center',                  
     },    
